33e8cf8da41af0e29546f7e27ad1465be6af7f03
takeshi0888 <woodstocks1955@gmail.com>
2020-08-28 12:30:47 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,16 @@ class User < ApplicationRecord
         has_many :comments
         has_many :orders
 
-        validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: '半角英数字を使用してください' }, uniqueness: true
-        validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6}/i, message: 'パスワードは半角英数字で６文字以上入力して下さい' }
+        validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は半角英数字を使用してください' }, uniqueness: true
+        validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i, message: 'は半角英数字で６文字以上入力して下さい' }
         validates :nickname, presence: true
-       
-        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+        validates :date_of_birth, presence: true
+
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
           validates :first_name
           validates :family_name
         end
-        with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' } do
+        with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナ文字を使用してください' } do
           validates :first_name_kana
           validates :family_name_kana
         end
