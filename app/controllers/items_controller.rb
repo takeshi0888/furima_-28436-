c2001e0091a76f516f587f2972adbe922efd90ab
takeshi0888 <woodstocks1955@gmail.com>
2020-08-31 17:24:30 +0900

    バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,27 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
-  def edit
-  end
-
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else render action: :new
+    end
   end
 
-  def show
+  def price
+    @price = price(price_params)
   end
 
+  private
 
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
 end
